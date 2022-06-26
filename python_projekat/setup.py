e1ad40f58caaa30b17e5a22ad27fce7c951f3e9c
Aleksandar Lukic <aleksandar.lukic@rt-rk.com>
2022-06-26 23:05:09 +0200

    ALU1:
    Stuff for building solution as library

--- python_projekat/setup.py
+++ python_projekat/setup.py
@@ -1,5 +1,22 @@
+import setuptools
 from distutils.core import setup, Extension
 
 setup(name="hash_module", version='1.0',
       ext_modules=[Extension('hash_module',
                              ['hash.c'])])
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+        name='group_1',
+        packages=setuptools.find_packages(),
+        version='0.0.1',
+        long_description=long_description,
+        description='User reg/login',
+        author='group_1',
+        license='MIT',
+        package_dir={},
+        install_requires=['sys', 'time'],
+        test_suite='tests',
+        python_requires=">=3.6",
+    )
